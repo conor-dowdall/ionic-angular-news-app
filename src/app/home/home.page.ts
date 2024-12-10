e9212bba21453540369dee8936b2a4d1c9d465f7
Conor Dowdall <conordowdallmail@gmail.com>
2024-12-10 21:19:07 +0000

    add styled search bar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonInput, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+countryName: string = "";
   constructor() {}
 }
