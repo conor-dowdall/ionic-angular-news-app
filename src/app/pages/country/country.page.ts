5a9dfe2e31601a8a0a56186693d9d0f2f88ad5b4
Conor Dowdall <conordowdallmail@gmail.com>
2024-12-15 13:51:03 +0000

    get country page working

--- src/app/pages/country/country.page.ts
+++ src/app/pages/country/country.page.ts
@@ -1,13 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import {
   IonContent,
   IonHeader,
   IonTitle,
   IonToolbar,
+  IonCard,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardHeader,
+  IonCardContent,
+  IonButton,
+  IonIcon,
 } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
+import { CountriesService, Country } from 'src/app/services/countries.service';
+import { addIcons } from 'ionicons';
+import { newspaperOutline, sunnyOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-country',
@@ -15,21 +24,42 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./country.page.scss'],
   standalone: true,
   imports: [
+    IonIcon,
+    IonButton,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCard,
     IonContent,
     IonHeader,
     IonTitle,
     IonToolbar,
     CommonModule,
-    FormsModule,
   ],
 })
 export class CountryPage implements OnInit {
-  countryName: string | null = null;
+  countryObject: Country | null = null;
 
   private route = inject(ActivatedRoute);
+  private countriesService = inject(CountriesService);
+
+  constructor() {
+    addIcons({ newspaperOutline, sunnyOutline });
+  }
 
   ngOnInit() {
-    this.countryName = this.route.snapshot.paramMap.get('countryName');
-    console.log('Country Name:', this.countryName);
+    const countryName = this.route.snapshot.paramMap.get('countryName');
+    this.loadCountryObject(countryName);
+  }
+
+  private async loadCountryObject(countryName: string | null) {
+    if (countryName != null) {
+      const countries = await this.countriesService.getCountries();
+      const country = countries.find(
+        (country) => country.name.official === countryName
+      );
+      if (country != null) this.countryObject = country;
+    }
   }
 }
